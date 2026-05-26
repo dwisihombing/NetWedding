251574f927c4070bb74176cbe091c4a84732eae5
Dwi Sihombing <35620333+dwisihombing@users.noreply.github.com>
2026-05-26 16:08:54 +0700

    fdd

--- components/WhoIsWatching.tsx
+++ components/WhoIsWatching.tsx
@@ -61,8 +61,8 @@ export default function WhoIsWatching({ onSelect, guestData }: WhoIsWatchingProp
                   <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z" />
                 </svg>
               ) : profile.avatar ? (
-                /* eslint-disable-next-line @next/next/no-img-element */
-                <>
+                <div className="relative w-full h-full">
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
                   <img 
                     src={profile.avatar} 
                     alt={profile.name} 
@@ -70,12 +70,12 @@ export default function WhoIsWatching({ onSelect, guestData }: WhoIsWatchingProp
                     onError={(e) => {
                       e.currentTarget.style.display = 'none';
                       if (e.currentTarget.nextElementSibling) {
-                        e.currentTarget.nextElementSibling.classList.remove('hidden');
+                        (e.currentTarget.nextElementSibling as HTMLElement).style.display = 'flex';
                       }
                     }}
                   />
-                  <span className="hidden">{profile.initial}</span>
-                </>
+                  <span className="hidden absolute inset-0 items-center justify-center text-6xl md:text-7xl font-bold">{profile.initial}</span>
+                </div>
               ) : (
                 profile.initial
               )}
