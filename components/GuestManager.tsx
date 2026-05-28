74e43c8d8584933f234a7fbd971aa75e1c41c22c
dwisihombing <antoshimura@gmail.com>
2026-05-28 18:09:18 +0700

    gfg

--- components/GuestManager.tsx
+++ components/GuestManager.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { createClient } from '@/lib/supabase/client'
+
 
 export function GuestManager() {
   const [guests, setGuests] = useState<any[]>([])
